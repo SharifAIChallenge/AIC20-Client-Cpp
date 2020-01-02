1a812e7da233e83a3c3af9a8dd27d6fc38ffda88
dot_blue <onajafi141@gmail.com>
2020-01-02 23:23:15 +0330

    Implement Pick Message

---
+++ client/src/Core/Message/Create/CreatePickMessage.h
@@ -0,0 +1,18 @@
+#ifndef AIC20_CLIENT_CPP_CREATEPICKMESSAGE_H
+#define AIC20_CLIENT_CPP_CREATEPICKMESSAGE_H
+
+#include "Core/Message/Create/EventMessage.h"
+
+class CreatePickMessage final: public EventMessage{
+public:
+
+    inline explicit CreatePickMessage(int id0,int id1,int id2,int id3,
+                                      int id4,int id5,int id6,int id7,
+                                      int id8):
+            EventMessage("pick",{id0,id1,id2,id3,id4,id5,id6,id7,id8})
+    {
+    }
+
+};
+
+#endif //AIC20_CLIENT_CPP_CREATEPICKMESSAGE_H
